482b2fd3fa9563f94e2eaaf68b7ad71fd0242cb3
Gabriel300p <gabriel.andrade200p@gmail.br>
2022-01-03 06:52:32 -0300

    1.4 - Making the center

--- components/SideBar.js
+++ components/SideBar.js
@@ -8,12 +8,15 @@ import {
 } from "@heroicons/react/outline";
 import { signOut, useSession } from "next-auth/react";
 import { useEffect, useState } from "react";
+import { useRecoilState } from "recoil";
+import { playlistIdState } from "../atoms/playlistAtom";
 import useSpotify from "../hooks/useSpotify";
 
 export default function SideBar() {
   const spotifyApi = useSpotify();
   const { data: session, status } = useSession();
   const [playlists, setPlaylists] = useState([]);
+  const [playlistsId, setPlaylistsId] = useRecoilState(playlistIdState);
 
   useEffect(() => {
     if (spotifyApi.getAccessToken()) {
@@ -24,7 +27,7 @@ export default function SideBar() {
   }, [session, spotifyApi]);
 
   return (
-    <div className="text-gray-500 p-5 text-sm border-r border-gray-900 overflow-y-scroll scrollbar-hide h-screen">
+    <div className="text-gray-500 p-5 text-xs lg:text-sm border-r border-gray-900 overflow-y-scroll scrollbar-hide h-screen sm:max-w-[12rem] lg:max-w-[15rem] hidden md:inline-flex">
       <div className="space-y-4">
         {/* First Section */}
         <button 
@@ -64,7 +67,11 @@ export default function SideBar() {
 
         {/* Playlists... */}
         {playlists.map((playlist) => (
-          <p key={playlist.id} className="cursor-pointer hover:text-white">
+          <p 
+            key={playlist.id} 
+            onClick={() => setPlaylistsId(playlist.id)}
+            className="cursor-pointer hover:text-white"
+          >
             {playlist.name}
           </p>
         ))}
